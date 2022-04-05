5cfb2fdb511fd23f05660cf7b23fd3c026c39a67
Alice Braye <braye.alice@gmail.com>
2022-04-05 17:14:30 +0200

    ok

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -5,13 +5,12 @@ import '../styles/PlantItem.css'
 function PlantItem() {
 
 	return (
-		< div className='plant-item'>
-			<div className='plant-item-cover'>
-				<img src="https://images.unsplash.com/photo-1648737119247-e93f56878edf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" alt="fausse image"></img>
-			</div>
-			<div className='plant-item-price'>15€</div>
+		<li className='plant-item'>
+			<span className='plant-item-price'>15€</span>
+				<img className='plant-item-cover' src="https://images.unsplash.com/photo-1648737119247-e93f56878edf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" alt="fausse image"></img>
+			
 			<h1>Belle Plante</h1>
-		</div>
+		</li>
 	)
 }
 
